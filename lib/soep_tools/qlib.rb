4cd42ffdad325d302bb742bf640f3ff5d0f94852
mhebing <mhebing@diw.de>
2013-06-07 16:27:00 +0200

    Minor update

--- lib/soep_tools/qlib.rb
+++ lib/soep_tools/qlib.rb
@@ -3,3 +3,12 @@ require "qlib/questionnaire"
 require "qlib/item"
 require "qlib/helper"
 
+##
+# Preparing questionnaires in XML format from QLIB for the import in
+# DDI on Rails and combine relevant information with existing structure
+# tables.
+#
+module SoepTools::QLIB
+
+end
+
