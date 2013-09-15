7ec72411243959fa4218277c409f92d7e11699c2
mhebing <mhebing@diw.de>
2013-09-15 19:26:56 +0200

    minor change, but it works
    (The solution to the privous problem was solved by
    uninstalling and reinstalling the gem)

--- lib/soep_tools/helper/latex_helper.rb
+++ lib/soep_tools/helper/latex_helper.rb
@@ -1,13 +1,18 @@
+module SoepTools::Helper
 
-module SoepTools::Helper::LatexHelper
-  def l string
-    string.gsub! /\\/, "XXX"
-    string.gsub! /&quot;/, " "
-    string.gsub! /&/, "\\&"
-    string.gsub! /_/, "\\_"
-    string.gsub! /%/, "\\%"
-    string.gsub! /<br>/, "\\\\\\\\"
-    string
+  ##
+  # Helper methods for classes working with LaTeX.
+  #
+  module LatexHelper
+    def l string
+      string.gsub! /\\/, "XXX"
+      string.gsub! /&quot;/, " "
+      string.gsub! /&/, "\\&"
+      string.gsub! /_/, "\\_"
+      string.gsub! /%/, "\\%"
+      string.gsub! /<br>/, "\\\\\\\\"
+      string
+    end
   end
 end
 
